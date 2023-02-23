da2144aece86e0365bab29bf1c43bdcfefa52d0b
Sejati Adli <sejatiadli@gmail.com>
2023-02-23 09:21:17 +0700

    [SA] hilangin timestamp di item dan game table

--- database/migrations/2023_01_09_032424_create_items_table.php
+++ database/migrations/2023_01_09_032424_create_items_table.php
@@ -19,7 +19,6 @@ return new class extends Migration
             $table->string('item');
             $table->integer('price');
             $table->string('pf_item');
-            $table->timestamps();
         });
     }
 
